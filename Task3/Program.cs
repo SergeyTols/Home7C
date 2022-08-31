67f533cfb2db855457468d7e94405b567f5c547d
Sarge <sarge2033@gmail.com>
2022-08-31 19:08:03 +0300

    Не получилось в массив красиво собрать. Не красиво не интересно

---
+++ Task3/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Write("Введите количество строк (m): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов (n): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+createArray(array);
+WriteArray(array);
+
+
+void createArray(int[,] array)
+{
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void WriteArray(int[,] array)
+{
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            int number = array[i, j];
+            Console.Write(number + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Cреднее арифметическое столбцов: ");
+for (int j = 0; j < n; j++)
+{
+    double average = 0;
+        for (int i = 0; i < m; i++)
+        {
+            average += array[i, j];
+        }
+    average = Math.Round(average / m, 1);
+    Console.Write(average + "  ");
+}
